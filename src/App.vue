c07e4abe18d46d32be27de17fee86c97ca5d6bfc
Velasco Carlos <carlos.velasco@afconsult.com>
2019-02-16 16:50:21 +0100

    vuex installed

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,87 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer />
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
+  <v-app
+    id="sandbox"
+    :dark="interfaceOptions.dark"
+  >
+    <v-navigation-drawer
+      v-model="interfaceOptions.primaryDrawer.model"
+      :permanent="interfaceOptions.primaryDrawer.type === 'permanent'"
+      :temporary="interfaceOptions.primaryDrawer.type === 'temporary'"
+      :clipped="interfaceOptions.primaryDrawer.clipped"
+      :floating="interfaceOptions.primaryDrawer.floating"
+      :mini-variant="interfaceOptions.primaryDrawer.mini"
+      absolute
+      overflow
+      app
+    >
+      <v-list two-line>
+        <v-subheader inset>
+          Settings
+        </v-subheader>
 
+        <v-list-tile
+          avatar
+          to="home"
+          :interface-options="interfaceOptions"
+        >
+          <v-list-tile-avatar>
+            <v-icon>settings</v-icon>
+          </v-list-tile-avatar>
+          <v-list-tile-content>
+            <v-list-tile-title>Interface </v-list-tile-title>
+            <v-list-tile-sub-title>Display options</v-list-tile-sub-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile
+          avatar
+          to="about"
+        >
+          <v-list-tile-avatar>
+            <v-icon>settings</v-icon>
+          </v-list-tile-avatar>
+          <v-list-tile-content>
+            <v-list-tile-title>About</v-list-tile-title>
+            <v-list-tile-sub-title>Carlos</v-list-tile-sub-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar
+      app
+      absolute
+      :clipped-left="interfaceOptions.primaryDrawer.clipped"
+    >
+      <v-toolbar-side-icon
+        v-if="interfaceOptions.primaryDrawer.type !== 'permanent'"
+        @click.stop="interfaceOptions.primaryDrawer.model = !interfaceOptions.primaryDrawer.model"
+      />
+      <v-toolbar-title>Carlos Velasco - Frontend Developer</v-toolbar-title>
+    </v-toolbar>
     <v-content>
-      <HelloWorld />
+      <v-container fluid>
+        <v-layout
+          align-center
+          justify-center
+        >
+          <v-flex xs10>
+            <router-view />
+          </v-flex>
+        </v-layout>
+      </v-container>
     </v-content>
+    <v-footer
+      :inset="interfaceOptions.footer.inset"
+      app
+    >
+      <span class="px-3">&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data () {
-    return {
-      //
+  export default {
+    data: () => ({
     }
+    )
   }
-}
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,86 @@
 <template>
-  <HelloWorld />
+  <v-card>
+    <v-card-text>
+      <v-layout
+        row
+        wrap
+      >
+        <v-flex
+          xs12
+          md6
+        >
+          <span>Scheme</span>
+          <v-switch
+            v-model="store.state.dark"
+            primary
+            label="Dark"
+            @click="
+              store.commit('changeDarkness', !store.state.dark)"
+          />
+        </v-flex>
+        <v-flex
+          xs12
+          md6
+        >
+          <span>Drawer</span>
+          <v-radio-group
+            v-model="interfaceOptions.primaryDrawer.type"
+            column
+          >
+            <v-radio
+              v-for="drawer in drawers"
+              :key="drawer"
+              primary
+              :label="interfaceOptions.drawer"
+              :value="interfaceOptions.drawer.toLowerCase()"
+            />
+          </v-radio-group>
+          <v-switch
+            v-model="interfaceOptions.primaryDrawer.clipped"
+            label="Clipped"
+            primary
+          />
+          <v-switch
+            v-model="interfaceOptions.primaryDrawer.floating"
+            label="Floating"
+            primary
+          />
+          <v-switch
+            v-model="interfaceOptions.primaryDrawer.mini"
+            label="Mini"
+            primary
+          />
+        </v-flex>
+        <v-flex
+          xs12
+          md6
+        >
+          <span>Footer</span>
+          <v-switch
+            v-model="interfaceOptions.footer.inset"
+            label="Inset"
+            primary
+          />
+        </v-flex>
+      </v-layout>
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer />
+      <v-btn flat>
+        Cancel
+      </v-btn>
+      <v-btn
+        flat
+        color="primary"
+      >
+        Submit
+      </v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
-
 <script>
-import HelloWorld from '../components/HelloWorld'
-
-export default {
-  components: {
-    HelloWorld
+  export default {
+    interfaceOptions: { footer: { inset: true }, drawer: {}, primaryDrawer: { mini: true, floating: true, clipped: true } }
   }
-}
+
 </script>
